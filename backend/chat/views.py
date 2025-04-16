f37704e533dae6c2b423e445862f7164e6f52c86
IrisStartupLab <fernando.dorantes@elektra.com.mx>
2025-04-16 14:40:31 -0600

    Añadiendo serializers y mejoras al código en el backend

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # backend/chat/views.py
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .scripts.gemini_connector import obtener_respuesta_gemini
+from chat.scripts.gemini_connector import obtener_respuesta_gemini
 from .serializers import RespuestaGeminiSerializer
 from .models import RespuestaGemini
 
