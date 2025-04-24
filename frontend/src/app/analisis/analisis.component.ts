444473328f842658b069e02145275e1a280cedf3
IrisStartupLab <fernando.dorantes@elektra.com.mx>
2025-04-24 11:11:15 -0600

    Añadiendo el esqueleto del frontend

---
+++ frontend/src/app/analisis/analisis.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analisis',
+  imports: [],
+  template: `
+    <p>
+      analisis works!
+    </p>
+  `,
+  styles: ``
+})
+export class AnalisisComponent {
+
+}
---
+++ frontend/src/app/scraping/scraping.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scraping',
+  imports: [],
+  template: `
+    <p>
+      scraping works!
+    </p>
+  `,
+  styles: ``
+})
+export class ScrapingComponent {
+
+}
